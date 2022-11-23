464e43036966bc188565c08211418bce9e2f52fa
jheasley02 <heaslejn@mail.uc.edu>
2022-11-22 23:55:57 -0500

    updated Part 5

--- tests/3.rock_paper_scissors-logic.test.js
+++ tests/3.rock_paper_scissors-logic.test.js
@@ -1,4 +1,5 @@
 const { RockPaperScissors } = require(`../resources/scripts/rock_paper_scissors.js`);
+const mathRandomSpy = jest.spyOn(Math, `random`);
 
 describe(`RockPaperScissors class`, function () {
   describe(`determineWinner()`, function () {
@@ -15,5 +16,22 @@ describe(`RockPaperScissors class`, function () {
     test(`lost cases`, function () {
       // Write your test here
     });
+  });
+  
+  describe(`generateCPUResponse()`, function () {
+    it(`Math.Random = 0.1 -> Rock`, function() {
+      mathRandomSpy.mockImplementation(() => 0.1);
+      const game = new RockPaperScissors();
+      expect(game.generateCPUResponse()).toBe(`rock`);
+    });
+
+    it(`Math.Random = 0.5 -> Paper`, function() {
+      // Write your test here
+    });
+
+    it(`Math.Random = 0.9 -> Paper`, function() {
+      // Write your test here
+    });
+
   });
 });
